c78d9621d4bcfe295af203d438c0f6dd7393ad15
wildarte <wildarte10@gmail.com>
2022-05-28 19:24:15 -0300

    criei o modal do simulador e ja fiz a versão mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,39 @@ btn_simulador.addEventListener('click', () => {
     modal_sim.classList.add('modal_board_open');
     board_simulador.classList.add('d-flex');
 
+    setTimeout(() => {
+        board_simulador.classList.add('board_simulador_open');
+    }, 10);
+
 });
 
-modal_sim.addEventListener('click', () => {
-    modal_sim.classList.remove('modal_board_open');
-    board_simulador.classList.remove('d-flex');
+modal_sim.addEventListener('click', (e) => {
+
+    //caso o elemento pai tenha sido clicado
+    if (e.target.nodeType == 1 && e.target.classList.contains('modal_board')){
+        
+        board_simulador.classList.remove('board_simulador_open'); 
+
+        setTimeout(() => {
+            modal_sim.classList.remove('modal_board_open');
+            board_simulador.classList.remove('d-flex');
+        }, 200);
+    }
+
+    /*
+    board_simulador.classList.remove('board_simulador_open');
+    document.querySelector('body').classList.remove('modal_open');
+
+    setTimeout(() => {
+        modal_sim.classList.remove('modal_board_open');
+        board_simulador.classList.remove('d-flex');
+    }, 200);
+    */
+
+});
+
+board_simulador.addEventListener('click', (e) => {
+
+    
+
 });
